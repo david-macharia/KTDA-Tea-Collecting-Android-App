f2e35b4d2200a4aa79bd0d04eb9341754c592bee
David <davidkiarie5861@gmail.com>
2018-05-26 20:24:57 +0300

    VERSON TWO

--- app/src/main/java/forthall/synergy/dansonmbuthia/application/ktdainterfaces/SettingActivity.java
+++ app/src/main/java/forthall/synergy/dansonmbuthia/application/ktdainterfaces/SettingActivity.java
@@ -2,6 +2,7 @@ package forthall.synergy.dansonmbuthia.application.ktdainterfaces;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -17,7 +18,7 @@ import forthall.synergy.SettingClasses.Setting;
  */
 public class SettingActivity extends AppCompatActivity {
 private CustomAdapter adapter;
-    private ListView listView;
+    private RecyclerView recycler;
     Setting settings;
     @Override
     public void onCreate(Bundle savedinstancebundles){
@@ -31,10 +32,9 @@ private CustomAdapter adapter;
                 new Setting("Always use current date",new CheckBox(this)),
                 new Setting("Always use current date",new CheckBox(this))};
         adapter= new CustomAdapter(this, R.layout.setting_row,settings);
-        listView=(ListView)findViewById(R.id.setting_listview);
-        listView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        recycler=(RecyclerView) findViewById(R.id.setting_listview);
+        recycler.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
 
-        listView.setAdapter(adapter);
 
     }
 }
